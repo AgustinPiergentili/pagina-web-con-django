dfa424046735af2ddeda2d2fb3d798cce04a0d71
Piergen <agus.piergentili73@gmail.com>
2022-12-04 21:27:37 -0300

    Se agrega login y register

--- EntregaIntermedia_Piergentili/account/urls.py
+++ EntregaIntermedia_Piergentili/account/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     path('crear_reseña/', CrearReseña,name='Crear Reseña'),
     path('crear_categoria/', CrearCategoria,name='Crear Categoria'),
     path('crear_autor/', CrearAutor,name='Crear Autor'),
-    path('buscar_reseña/', buscar_reseña,name='Buscar Reseña'),
+    path('buscar_resena/', buscar_reseña,name='Buscar Reseña'),
     path('reseña_list/', ReseñaList.as_view(),name='List'),
     path('reseña_detail/<pk>', ReseñaDetail.as_view(),name='Detail'),
     path('reseña_confirm_delete/<pk>', ReseñaDelete.as_view(),name='Delete'),
     path('reseña_edit/<pk>', ReseñaUpdate.as_view(),name='Update'),
+    path('login/',login_request,name='Login'),
+    path('registro/',register,name='Register'),
 ]
--- EntregaIntermedia_Piergentili/account/views.py
+++ EntregaIntermedia_Piergentili/account/views.py
@@ -5,7 +5,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from account.models import *
 from .forms import CrearReseñaForm,CategoriaForm,AutorForm
-
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 
@@ -79,16 +80,65 @@ def CrearAutor(request):
     return render(request,'account/crearautor.html',{'formulario3':formulario3})
 
 
+
 def buscar_reseña(request):
 
-    if request.GET.get('reseña', False):
-        reseña = request.GET['reseña']
-        reseñas = Reseña.objects.filter(nombre__icontains=reseña)
+    if request.GET.get('resena', False):
+        resena = request.GET['resena']
+        resenas = Reseña.objects.filter(nombre__icontains=resena)
 
-        return render(request,'account/buscar_reseña.html', {'reseñas':reseñas})
+        return render(request,'account/buscar_resena.html', {'resenas':resenas})
     else:
         respuesta = 'No hay datos'
-    return render(request, 'account/buscar_reseña.html', {'respuesta':respuesta})
+    return render(request, 'account/buscar_resena.html', {'respuesta':respuesta})
+
+
+
+def login_request(request):
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+
+        if form.is_valid():
+            usuario = form.cleaned_data.get('username')
+            contra = form.cleaned_data.get('password')
+
+            user = authenticate(username=usuario, password=contra)
+
+            if user is not None:
+                login(request,user)
+
+                return render(request,'account/index.html',{'mensaje':f'Bienvenido {usuario}'})
+            else:
+                return render(request,'account/index.html',{'mensaje':'Error, datos incorrectos'})
+        
+        else:
+
+                return render(request,'account/index.html',{'mensaje':'Error, formulario erroneo'})
+
+    form = AuthenticationForm()
+
+    return render(request,'account/login.html', {'form':form})
+
+
+
+def register(request):
+    if request.method == 'POST':
+
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+
+            username = form.cleaned_data['username']
+            form.save()
+            return render(request,'account/index.html', {'mensaje':'Usuario Creado'})
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'account/registro.html', {'form':form})
+    
+
 
 
 class ReseñaList(ListView):
